8f9335b41bee6d4b2abe4b81db9cf7455ead0fe3
Aditya <aditya23@gmail.com>
2025-12-26 06:31:24 +0000

    Fixed another error

--- src/controllers/service.controller.js
+++ src/controllers/service.controller.js
@@ -1,7 +1,22 @@
 const Service = require('../models/Service');
 
 exports.getServices = async (req, res) => {
-  const services = await Service.find();
-  res.render('pages/services', { services });
+  try {
+    const services = await Service.getAll(); // <-- use getAll() for MySQL
+    res.render('pages/services', { services });
+  } catch (err) {
+    console.error('DB Error:', err.message);
+    res.status(500).send('Database error');
+  }
+};
+
+exports.createService = async (req, res) => {
+  try {
+    const service = await Service.create(req.body); // optional create endpoint
+    res.json(service);
+  } catch (err) {
+    console.error('DB Error:', err.message);
+    res.status(500).json({ error: err.message });
+  }
 };
 
